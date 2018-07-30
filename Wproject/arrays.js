262e4a1942d54b4afde49fb3b98f012c23d38ac4
cesar <cesarbworkmail@gmail.com>
2018-07-30 20:43:49 +0100

    testing code, in test.js, organizing with comments, adding buttons and format for a clear view

--- Wproject/arrays.js
+++ Wproject/arrays.js
@@ -22,10 +22,12 @@ function myFunction() {
 
 */
 
-var gNames = ['alejandra','martina','josefa','renata','francis'];
-var ages = [18,19,20,21,50];
-var Career = ['graphic design','nursing','reporter','police officer','politician'];
+var gNames = ['alejandra','martina','josefa','renata','francis','mariana'];
+var ages = [18,19,20,21,50,45];
+var Career = ['graphic design','nursing','reporter','police officer','politician','curator'];
 
+
+//using random to mix all info in arrays
 
 function my_gNames() {
     gNames.sort(function(a, b){return 0.5 - Math.random()});
@@ -43,7 +45,7 @@ my_gNames();
 my_ages();
 my_Career();
 
-
+// -- not working mix is not an array
 var i;
 var mix='';
 
@@ -51,25 +53,28 @@ for( i= 0; i < gNames.length; i++)
     mix += (gNames[i]+ ' ' + ages[i]+ ' ' + Career[i]) + '\n';
 
 
-console.log(mix);
+//console.log(mix);
 
 
-var item = gNames[Math.floor(Math.random()*gNames.length)];
-console.log(item + '\n');
+//var item = gNames[Math.floor(Math.random()*gNames.length)];
+
+//console.log(item + '\n');
 
 console.log(gNames + '\n');
 
 console.log(ages[1]+'\n');
 
-const object1 = {
+//sample of an object
+
+const persona = {
   name: gNames[0],
   age: ages[0],
   prof: Career[0]
 };
 
-console.log(Object.values(object1)+'\n');
-console.log(object1.a + '\n');
-console.log(object1);
+console.log(Object.values(persona)+'\n');
+console.log(persona.age + '\n');
+console.log(persona + '\n');
 
 //constructor
 
@@ -83,25 +88,39 @@ var me = new Person('cesar','bernal', 35);
 
 console.log(me.name_p + '\n');
 
-var woman = new Person(gNames[0],Career[0],ages[0]);
-console.log(woman);
+var womanTest = new Person(gNames[0],Career[0],ages[0]);
+console.log(womanTest);
 console.log('\n');
 
 
-
-var man = [];
+//the working instruction to create the objects array
+var woman = [];
 function objectsInArray() {
     var j;
     for(j = 0; j < gNames.length; j++)
-        man.push(new Person(gNames[j],Career[j],ages[j]));
-        console.log(man + '\n');
+        woman.push(new Person(gNames[j],Career[j],ages[j]));
+        console.log(woman + '\n');
 }
 
 //function to unpack the objects
+//Course Working With External Resources Data & The DOM Unpacking Our Data Onto The DOM
+//second video
 
-function writeToDocumentExtra() {
-        man.forEach(function (item) {
-            document.getElementById('demo').innerHTML = item.name_p;
+function writeToDocumentNames() {
+        woman.forEach(function (item) {
+            document.getElementById('demo').innerHTML += '<p>' + item.name_p + '</p>';
+        });
+}
+
+function writeToDocumentCareers() {
+        woman.forEach(function (item) {
+            document.getElementById('demo1').innerHTML += '<p>' + item.prof_p + '</p>';
+        });
+}
+
+function writeToDocumentAges() {
+        woman.forEach(function (item) {
+            document.getElementById('demo2').innerHTML += '<p>' + item.age_p + '</p>';
         });
 }
 
---
+++ Wproject/test.js
@@ -0,0 +1,15 @@
+
+var gNames = ['alejandra','martina','josefa','renata','francis','mariana'];
+var ages = [18,19,20,21,50,45];
+var Career = ['graphic design','nursing','reporter','police officer','politician','curator'];
+
+
+const person = {
+  name: gNames[0],
+  age: ages[0],
+  prof: Career[0]
+};
+
+console.log(Object.values(person)+'\n');
+console.log(person.age + '\n');
+console.log(person + '\n');
