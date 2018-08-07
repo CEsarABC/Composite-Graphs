edd9f234e954ac28f54beec056fe07b4d129bb22
cesar <cesarbworkmail@gmail.com>
2018-08-07 17:33:31 +0100

    the use of the array of objects is successful the graph is showing the values in each of the objects, positioning the big chart was not possible at the end of the page for some reason the layout was giving problems..

--- Wproject/arrays.js
+++ Wproject/arrays.js
@@ -177,3 +177,73 @@ function newInArray() {
 
 newInArray();
 
+
+/////////////////////////////big test
+
+
+
+
+    var h = 450;
+    var w = 600;
+    var barPadding = 2;
+    var lenghtWoman = woman.length;
+    console.log(lenghtWoman);
+    var colWidth = w / woman.length; //gives the size the space divided by the number of objects
+    var barwidth = colWidth - barPadding; //gives the total width for each bar
+    var salaryValues = [];
+
+    function salaryArray() {
+        for(u = 0; u < lenghtWoman; u++)
+            salaryValues[u] = woman[u].salary_p;
+        console.log(salaryValues);
+    }
+
+    salaryArray();
+
+function bigGraph() {
+    var scale = d3.scale.linear()//we need to apply this function to some of the values
+        /*.domain([0,600])*/
+        .domain([0, d3.max(salaryValues)])//we can use a function to give the max value in the array
+        .range([0,400]);
+
+    var svg = d3.select('#draw-chart')
+        .append('svg')
+        .attr('height', h)
+        .attr('width', w);
+    svg.selectAll('rect')
+        .data(salaryValues)
+        .enter()
+        .append('rect')
+        .attr('x', function (d ,i) {
+            return i*( w / lenghtWoman); //gives position the every element by multiplying the index
+        })
+        .attr('y', function (d) {
+            return h- scale(d);
+        })
+        .attr('height', function (d) {
+            return scale(d);
+        })
+        .attr('width', barwidth)
+        .attr('fill', 'green');
+
+    svg.selectAll('text')
+        .data(salaryValues)
+        .enter()
+        .append('text')
+        .text(function (d) {
+            return  d;
+        })
+        .attr('text-anchor','middle')
+        .attr('x', function (d,i) {
+            return (i * colWidth) + barwidth / 2;
+            //this function finds the exact middle point between position and width of the bar
+            //multiplies the index by the total space of the column and sums the barwidth to find the exact middle point by dividing by 2
+        })
+        .attr('y', function (d) {
+            return h - scale(d) + 14;
+        })
+        .attr('font-family','sans-serif')
+        .attr('font-size', '11px')
+        .attr('fill','white')
+
+}
